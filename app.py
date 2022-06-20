85941f3b6b91d9ec26141cb93a6c9fa16e0c440e
Avdhut Petkar <57283175+Avdhut2560@users.noreply.github.com>
2022-06-20 20:27:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -25,14 +25,14 @@ def man():
     return render_template('home.html')
 
 
-@app.route('/predicts', methods=['POST'])
+@app.route('/predict', methods=['POST'])
 def home():
     data1 = request.form['a']
     data2 = request.form['b']
     data3 = request.form['c']
     arr = np.array([[data1, data2, data3]])
     pred = model.predict(arr)
-    return render_template('after.html',data=pred)
+    return render_template('after.html',data =pred)
 
 
 if __name__ == "__main__":
