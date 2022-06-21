8b2d9f191c5623b0752c8417d4cc3cec25a0370e
Avdhut Petkar <57283175+Avdhut2560@users.noreply.github.com>
2022-06-21 20:18:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,6 @@ def home():
     data1 = request.form['a']
     data2 = request.form['b']
     data3 = request.form['c']
-    data4 = request.form['d']
     arr = np.array([[data1, data2, data3]])
     pred = model.predict(arr)
     return render_template('after.html', data=pred)
